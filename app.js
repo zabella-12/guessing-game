07f8f465735da3d7a2c271ece827555eb7ddd184
zabella-12 <issaasousa@outlook.com>
2025-08-01 17:05:17 -0300

    change speed voice

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
     if ('speechSynthesis' in window) {
         let utterance = new SpeechSynthesisUtterance(texto);
         utterance.lang = 'pt-BR';
-        utterance.rate = 1.2;
+        utterance.rate = 1.75;
         window.speechSynthesis.speak(utterance);
     } else {
         console.log("Web Speech API não suportada neste navegador.");
